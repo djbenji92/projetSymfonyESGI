faa9f53b82d0e9b2d47219b7fc5356976bf8d2b7
Benjamin ROUVILLE <benjamin.rouville@gmail.com>
2016-10-19 16:29:30 +0200

    add dispatcher for login

--- blog/src/BlogBundle/Controller/DefaultController.php
+++ blog/src/BlogBundle/Controller/DefaultController.php
@@ -14,4 +14,5 @@ class DefaultController extends Controller
     {
         return $this->render('BlogBundle:Default:index.html.twig');
     }
+
 }
---
+++ blog/src/BlogBundle/Controller/DispatcherController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class DispatcherController extends Controller
+{
+
+    /**
+     * show home page for user after login
+     *
+     * @Route("/utilisateur", name="utilisateur_home")
+     * @Method("GET")
+     */
+    public function pageUserAction()
+    {
+        return $this->render('dispatcher/utilisateur.html.twig');
+    }
+
+    /**
+     * show home page for redactor after login
+     *
+     * @Route("/redacteur", name="redacteur_home")
+     * @Method("GET")
+     */
+    public function pageRedacteurAction()
+    {
+        return $this->render('dispatcher/redacteur.html.twig');
+    }
+
+    /**
+     * show home page for admin after login
+     *
+     * @Route("/administrateur", name="admin_home")
+     * @Method("GET")
+     */
+    public function pageAdminAction()
+    {
+        return $this->render('dispatcher/admin.html.twig');
+    }
+
+    /**
+     * show home page for admin after login
+     *
+     * @Route("/dispatcher-login", name="dispatch_login")
+     * @Method("GET")
+     */
+    public function redirectAfterLogin(){
+      $securityContext = $this->container->get('security.authorization_checker');
+      if ($securityContext->isGranted('ROLE_ADMIN')) {
+          return $this->redirectToRoute('admin_home');
+      }
+      else if($securityContext->isGranted('ROLE_REDACTEUR')){
+          return $this->redirectToRoute('redacteur_home');
+      }
+      else{
+          return $this->redirectToRoute('utilisateur_home');
+      }
+
+
+    }
+
+}
