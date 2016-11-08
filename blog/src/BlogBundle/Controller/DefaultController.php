1ca6c02ae9a5ba6783cda8bb7f5e6c99120a27ab
Benjamin ROUVILLE <benjamin.rouville@gmail.com>
2016-11-08 16:53:33 +0100

    add categories in menu

--- blog/src/BlogBundle/Controller/DefaultController.php
+++ blog/src/BlogBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace BlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use BlogBundle\Entity\Category;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,6 +15,20 @@ class DefaultController extends Controller
     public function indexAction()
     {
         return $this->render('BlogBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @Route("/navigation-categ", name="navigation_categ")
+     */
+    public function navigationCategAction(){
+      $em = $this->getDoctrine()->getManager();
+      $categories = $em->getRepository('BlogBundle:Category')->findAll();
+
+      //return new Response('categories'->$categories);
+
+      return $this->render('template/navigationCategorie.html.twig', array(
+          'categories'=> $categories,
+      ));
     }
 
 }
